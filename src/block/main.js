a679349c7e0dd8dc6b004e3342574e451bb0a41c
klarityorg-dev <dev@klarity.org>
2019-05-30 10:30:23 +0700

    change css and js

--- src/block/main.js
+++ src/block/main.js
@@ -1,5 +1,15 @@
-jQuery( function ( $ ) {
-    $(document).ready(function () {
-
+document.addEventListener("DOMContentLoaded", function(event) {
+    //do work
+    jQuery( function ( $ ) {
+        $('.publication-share').click(function (e) {
+            e.preventDefault();
+            $(this).parent().find('.container-publication-share').toggle();
+        });
+        $('.publication-share').on( "mouseenter", function() {
+            $(this).parent().find('.container-publication-share').show();
+        })
+        .on( "mouseleave", function() {
+            $(this).parent().find('.container-publication-share').hide();
+        });
     });
 });
