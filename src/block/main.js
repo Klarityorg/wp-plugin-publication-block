f48f6ad070fcf0a59a75dcdfa3d66d7397a275fb
klarityorg-dev <dev@klarity.org>
2019-05-30 11:24:04 +0700

    change css and js

--- src/block/main.js
+++ src/block/main.js
@@ -1,15 +1,23 @@
 document.addEventListener("DOMContentLoaded", function(event) {
+    'use strict';
     //do work
     jQuery( function ( $ ) {
         $('.publication-share').click(function (e) {
             e.preventDefault();
-            $(this).parent().find('.container-publication-share').toggle();
+            if($(this).parent().find('.container-publication-share').hasClass("toggled-on"))
+            {
+                $(this).parent().find('.container-publication-share').removeClass("toggled-on");
+            }
+            else{
+                $(this).parent().find('.container-publication-share').addClass("toggled-on");
+            }
+
         });
         $('.publication-share').on( "mouseenter", function() {
-            $(this).parent().find('.container-publication-share').show();
+            $(this).parent().find('.container-publication-share').addClass("toggled-on");
         })
         .on( "mouseleave", function() {
-            $(this).parent().find('.container-publication-share').hide();
+            $(this).parent().find('.container-publication-share').removeClass("toggled-on");
         });
     });
 });
